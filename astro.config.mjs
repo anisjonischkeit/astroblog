be194bb85b4bac104f8a4b90e9da0ff4662b282c
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2023-08-20 17:00:02 +1000

    fix baseurl

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://anisjonischkeit.github.io",
   base: "/astroblog",
-
+  trailingSlash: "never",
   integrations: [mdx(), sitemap()],
 });
