5aeaf17816e31365d381d725130f3e269060b518
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2023-08-20 18:06:27 +1000

    site title

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "The Blog of Anis Jonischkeit";
+export const SITE_TITLE = "Anis Jonischkeit";
 export const SITE_DESCRIPTION = "Welcome to my website!";
